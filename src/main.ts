7ecc3090e5496ed5c303e86322c265a6c27bba10
franbz1 <franciscorualesp15@gmail.com>
2024-12-30 22:41:18 -0500

    Inicio de migracion products rest a microservice

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Products-App-Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    }
+  )
 
   app.useGlobalPipes(  
     new ValidationPipe({ 
@@ -16,7 +25,7 @@ async function bootstrap() {
     }) 
   );
 
-  await app.listen(envs.port);
-  logger.log(`App is running on port ${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservice is running on port ${envs.port}`);
 }
 bootstrap();
